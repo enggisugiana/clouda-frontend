624a90b522c88c82d3277f6a6524e6780533d40e
enggisugiana <enggi.sugiana@gmail.com>
2019-12-12 11:20:28 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+(function(){
+    $('#partner').on('click', function(){
+        console.log("status:ok");
+        // var tujuan = $(this).attr('href');
+    
+        // var elemenTujuan = $(tujuan);
+    
+        // $('html , body').animate({
+        //     scrollTop: elemenTujuan.offset().top - 50
+        // }, 'linear');
+    
+        // e.preventDefault();
+    
+    });
+});
